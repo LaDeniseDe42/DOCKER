df58b0a52e6424edf75f0a1c7f7cf0149cafc7f4
LaDeniseDe42 <quentin_deni@hotmail.fr>
2024-01-29 16:38:21 +0100

    OK

--- srcs/requirements/WordPress/conf/wp-autoconfig.sh
+++ srcs/requirements/WordPress/conf/wp-autoconfig.sh
@@ -6,13 +6,17 @@ sleep 10
 if [ -f /var/www/html/wp-config.php ]; then
 	echo "wordpress is already configured."
 	/usr/sbin/php-fpm7.4 -F
-fi
+else
 
 wp config create --allow-root --dbname=${DATABASE} --dbuser=${USER} --dbpass=${USER_PASSWORD} --dbhost=mariadb
 wp core install --allow-root --url=${HOSTNAME} --title="Inception" --admin_user=o5 --admin_password=${ROOT_PASSWORD} --admin_email=adm@qdenizar.42.fr
 wp user create --allow-root ${USER} ${USER}@${HOSTNAME} --role=author --user_pass=${USER_PASSWORD}
 wp plugin update --allow-root --all
 
+fi
+
+#wp theme activate my-custom-theme --allow-root
+#echo "define('WP_DEFAULT_THEME', 'my-custom-theme');" >> /var/www/html/wp-config.php
 echo "JOB done."
 
 /usr/sbin/php-fpm7.4 -F
