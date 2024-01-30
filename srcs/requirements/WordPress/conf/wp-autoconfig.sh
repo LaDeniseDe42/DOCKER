c3de0e3e041898304e0ffcfd0c4590d55ead3cb8
LaDeniseDe42 <quentin_deni@hotmail.fr>
2024-01-30 14:55:33 +0100

    ok

--- srcs/requirements/WordPress/conf/wp-autoconfig.sh
+++ srcs/requirements/WordPress/conf/wp-autoconfig.sh
@@ -9,7 +9,7 @@ if [ -f /var/www/html/wp-config.php ]; then
 else
 
 wp config create --allow-root --dbname=${DATABASE} --dbuser=${USER} --dbpass=${USER_PASSWORD} --dbhost=mariadb
-wp core install --allow-root --url=${HOSTNAME} --title="Inception" --admin_user=o5 --admin_password=${ROOT_PASSWORD} --admin_email=adm@qdenizar.42.fr
+wp core install --allow-root --url=${HOSTNAME} --title="Inception" --admin_user=MasterOfUniverse --admin_password=${ROOT_PASSWORD} --admin_email=adm@qdenizar.42.fr
 wp user create --allow-root ${USER} ${USER}@${HOSTNAME} --role=author --user_pass=${USER_PASSWORD}
 wp plugin update --allow-root --all
 
@@ -17,6 +17,9 @@ fi
 
 #wp theme activate my-custom-theme --allow-root
 #echo "define('WP_DEFAULT_THEME', 'my-custom-theme');" >> /var/www/html/wp-config.php
+
 echo "JOB done."
 
 /usr/sbin/php-fpm7.4 -F
+#Lance à nouveau le serveur PHP-FPM en mode "foreground".
+#Cela permet au conteneur de rester actif tant que PHP-FPM est en cours d'exécution.
